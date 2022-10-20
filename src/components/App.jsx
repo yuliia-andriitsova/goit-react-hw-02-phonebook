94954908b770dd6b04cf2e6d93661adbc2b4ddc3
yuliia-andriitsova <andriitsovayulia@gmail.com>
2022-10-20 22:10:48 +0300

    part 1-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,50 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { nanoid } from 'nanoid';
+import { Component } from 'react';
+import { PhoneBook } from './PhoneBook';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
+
+  onChangeName = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState(prevState => {
+      return {
+        contacts: [
+          ...prevState.contacts,
+          { name: this.state.name, number: this.state.number, id: nanoid() },
+        ],
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'flexStart',
+          alignItems: 'center',
+          fontSize: 25,
+          color: '#010101',
+          gap: 10,
+        }}
+      >
+        <PhoneBook
+          handleAddContact={this.handleSubmit}
+          onChangeName={this.onChangeName}
+          contacts={this.state.contacts}
+        />
+      </div>
+    );
+  }
+}
